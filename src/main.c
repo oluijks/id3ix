1620c5d7bd32dc1c6a4a7cfd701794768d9a77e2
Olaf Luijks <olafluijks@proton.me>
2026-08-02 19:27:27 +0000

    feat: handle basic CLI commands

--- src/main.c
+++ src/main.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void show_usage(void);
 
 int main(int argc, char **argv)
 {
-    printf("Number of arguments: %d\n", argc);
-
-    for (int i = 0; i < argc; i++)
+    if (argc < 2)
     {
-        printf("argv[%d] = %s\n", i, argv[i]);
+        show_usage();
+
+        return EXIT_FAILURE;
+    }
+
+    if (strcmp(argv[1], "scan") == 0)
+    {
+        printf("Scanning...\n");
+    }
+    else
+    {
+        printf("Unknown command\n");
     }
 
     return EXIT_SUCCESS;
+}
+
+void show_usage(void)
+{
+    printf("id3ix - metadata utility\n\n");
+    printf("Usage:\n");
+    printf("  id3ix scan <directory>\n");
 }
